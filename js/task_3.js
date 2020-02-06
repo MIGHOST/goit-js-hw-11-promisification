92fff4b677f56b4ce5061b5a1c8cb23cfb4cf7b4
Migho <mi.sidorenko@gmail.com>
2020-02-06 18:14:26 +0200

    corect_t_3

--- js/task_3.js
+++ js/task_3.js
@@ -1,24 +1,27 @@
-"use strict";
+// "use strict";
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = transaction => {
-  const delay = randomIntegerFromInterval(200, 500);
-return new Promise( (resolve, reject) => {
-  setTimeout(() => {
+const makeTransaction = (transaction) => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+    setTimeout(() => {
+      
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      resolve(transaction.id, delay);
+      resolve([transaction.id, delay]);
+  
     } else {
       reject(transaction.id);
     }
   }, delay)})};
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
+  
 };
 
 const logError = id => {
@@ -28,6 +31,7 @@ const logError = id => {
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
+ 
 
 makeTransaction({ id: 71, amount: 230 })
   .then(logSuccess)
@@ -40,3 +44,6 @@ makeTransaction({ id: 72, amount: 75 })
 makeTransaction({ id: 73, amount: 100 })
   .then(logSuccess)
   .catch(logError);
+
+
+
